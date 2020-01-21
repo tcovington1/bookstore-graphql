5786fc56cdddbab47113fdfd5f4c43ab6575e34f
Taylor Covington <tcovingtondev@gmail.com>
2020-01-20 20:34:54 -0700

    seeded faker data, created assets => config => manifest.js file,graphiQL is running.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,14 @@
 module Types
   class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: true
+    field :email, String, null: true
+    field :favorite_genre, String, null: true
+    field :books, [Types::BookType], null: true
+    field :books_count, Integer, null: true
+
+    def books_count
+      object.books.count
+    end
   end
 end
